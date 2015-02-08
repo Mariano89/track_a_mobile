7edbf6561719f4a435bd49d92334edb379d6b5c3
mariano89 <marianoe1989@gmail.com>
2015-02-08 01:21:34 -0800

    crete log in form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new', as: :signup
 
+  get '/login' => 'sessions#new', as: :login
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
+
   resources :users
   resources :cars
 
